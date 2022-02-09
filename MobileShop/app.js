b66b3cee3420bce69cb546d99995c4e0aa443dd4
fzksbalazs <fzksbalazs@gmail.com>
2022-02-09 17:06:28 +0100

    Adatbazis és backend

--- MobileShop/app.js
+++ MobileShop/app.js
@@ -1,25 +1,36 @@
-const path = require('path');
-
-const express = require('express');
-
-const errorController = require('./controllers/error');
-
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+// const stripeRoute = require("./routes/stripe");
+// const cors = require("cors");
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
-app.use(errorController.get404);
+dotenv.config();
 
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DBConnection Succesfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(3000);
+  // app.use(cors());
+  app.use(express.json());
+  app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
+  // app.use("/api/products", productRoute);
+  // app.use("/api/carts", cartRoute);
+  // app.use("/api/orders", orderRoute);
+  // app.use("/api/checkout", stripeRoute);
+  
+  app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running!");
+  });
+  
+ 
--- MobileShop/models/cart.js
+++ MobileShop/models/cart.js
@@ -1,74 +1,114 @@
-const fs = require("fs");
-const path = require("path");
+const mongoose = require("mongoose");
 
-const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+const CartSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-module.exports = class Cart {
-  static addProduct(id, productPrice) {
-    fs.readFile(p, (err, fileContent) => {
-      let cart = { products: [], totalPrice: 0 };
-      if (!err) {
-        cart = JSON.parse(fileContent);
-      }
+module.exports = mongoose.model("Cart", CartSchema);
 
-      const existingProduct = cart.products.find((prod) => prod.id === id);
-      const existingProductIndex = cart.products.findIndex(
-        (prod) => prod.id === id
-      );
-      let updatedProduct;
-      if (existingProduct) {
-        updatedProduct = { ...existingProduct };
-        updatedProduct.qty += 1;
-        cart.products = [...cart.products];
-        cart.products[existingProductIndex] = updatedProduct;
-      } else {
-        updatedProduct = { id: id, qty: 1 };
-        cart.products = [...cart.products, updatedProduct];
-      }
 
-      cart.totalPrice += +productPrice;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
-        console.log(err);
-      });
-    });
-  }
-  static getCart(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-          cb([]);
-      } else {
-          cb(JSON.parse(fileContent));
-      }
-    });
-  }
 
-  static deleteProduct(id, productPrice){
-    fs.readFile(p, (err, fileContent) => {
-      let cart = { products: [], totalPrice: 0 };
-      if (!err) {
-        cart = JSON.parse(fileContent);
-      }
-      const existingProduct = cart.products.find((prod) => prod.id === id);
-      const existingProductIndex = cart.products.findIndex(
-        (prod) => prod.id === id
-      );
-      let updatedProduct;
-        if(existingProduct.qty === 1)
-        {
-          cart.products = [...cart.products];
-          cart.products = cart.products.filter((prod) => prod.id !== id);
-          cart.totalPrice -= productPrice;
-        }
-      else {
-        updatedProduct = { ...existingProduct };
-        updatedProduct.qty -= 1;
-        cart.products = [...cart.products];
-        cart.products[existingProductIndex] = updatedProduct;
-        cart.totalPrice -= productPrice;
-      }
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
-        console.log(err);
-      });
-    });
-  }
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require("fs");
+// const path = require("path");
+
+// const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+
+// module.exports = class Cart {
+//   static addProduct(id, productPrice) {
+//     fs.readFile(p, (err, fileContent) => {
+//       let cart = { products: [], totalPrice: 0 };
+//       if (!err) {
+//         cart = JSON.parse(fileContent);
+//       }
+
+//       const existingProduct = cart.products.find((prod) => prod.id === id);
+//       const existingProductIndex = cart.products.findIndex(
+//         (prod) => prod.id === id
+//       );
+//       let updatedProduct;
+//       if (existingProduct) {
+//         updatedProduct = { ...existingProduct };
+//         updatedProduct.qty += 1;
+//         cart.products = [...cart.products];
+//         cart.products[existingProductIndex] = updatedProduct;
+//       } else {
+//         updatedProduct = { id: id, qty: 1 };
+//         cart.products = [...cart.products, updatedProduct];
+//       }
+
+//       cart.totalPrice += +productPrice;
+//       fs.writeFile(p, JSON.stringify(cart), (err) => {
+//         console.log(err);
+//       });
+//     });
+//   }
+//   static getCart(cb) {
+//     fs.readFile(p, (err, fileContent) => {
+//       if (err) {
+//           cb([]);
+//       } else {
+//           cb(JSON.parse(fileContent));
+//       }
+//     });
+//   }
+
+//   static deleteProduct(id, productPrice){
+//     fs.readFile(p, (err, fileContent) => {
+//       let cart = { products: [], totalPrice: 0 };
+//       if (!err) {
+//         cart = JSON.parse(fileContent);
+//       }
+//       const existingProduct = cart.products.find((prod) => prod.id === id);
+//       const existingProductIndex = cart.products.findIndex(
+//         (prod) => prod.id === id
+//       );
+//       let updatedProduct;
+//         if(existingProduct.qty === 1)
+//         {
+//           cart.products = [...cart.products];
+//           cart.products = cart.products.filter((prod) => prod.id !== id);
+//           cart.totalPrice -= productPrice;
+//         }
+//       else {
+//         updatedProduct = { ...existingProduct };
+//         updatedProduct.qty -= 1;
+//         cart.products = [...cart.products];
+//         cart.products[existingProductIndex] = updatedProduct;
+//         cart.totalPrice -= productPrice;
+//       }
+//       fs.writeFile(p, JSON.stringify(cart), (err) => {
+//         console.log(err);
+//       });
+//     });
+//   }
+// };
