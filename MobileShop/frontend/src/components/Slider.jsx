021e718bf283bf92cabce61305b531b8fd9ff353
fzksbalazs <fzksbalazs@gmail.com>
2022-02-24 16:12:28 +0100

    Product design

--- MobileShop/frontend/src/components/Slider.jsx
+++ MobileShop/frontend/src/components/Slider.jsx
@@ -6,7 +6,7 @@ import { mobile } from "../responsive";
 
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 75vh;
   display: flex;
   position: relative;
   overflow: hidden;
--- MobileShop/frontend/src/pages/Home.jsx
+++ MobileShop/frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 
 import React from 'react'
 import Announcement from '../components/Announcement'
+import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
+import Products from '../components/Products'
 import Slider from '../components/Slider'
+
 
 const Home = () => {
   return (
@@ -10,6 +13,9 @@ const Home = () => {
         <Announcement/>
         <Navbar/>
         <Slider/>
+        <Categories/>
+        <Products/>
+        
     </div>
   )
 }
