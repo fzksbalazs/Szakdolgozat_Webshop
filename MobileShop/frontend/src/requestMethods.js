744ac81624e0b8a5b8e6623285a23413f35858af
fzksbalazs <meronka.patrik@students.jedlik.eu>
2022-03-20 16:00:44 +0100

    statisztikak beallitasa

--- MobileShop/frontend/src/requestMethods.js
+++ MobileShop/frontend/src/requestMethods.js
@@ -5,13 +5,9 @@ const BASE_URL = "http://localhost:5000/api/";
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
 
-const TOKEN = 
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMDQyMjM1OTNjOTg0YzI2YTE4ZmQ1YSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0Njg0MDcxNSwiZXhwIjoxNjQ3MDk5OTE1fQ.YGu3MD6vAbYHCXgHeUBIjT8GLIFM53_QonFbqTItvs4"
-
-
-// const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
-// const currentUser = user && JSON.parse(user).currentUser;
-// const TOKEN = currentUser?.accessToken;
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -19,6 +15,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
-  
+  headers: { token: `Bearer ${TOKEN}` },
 });
